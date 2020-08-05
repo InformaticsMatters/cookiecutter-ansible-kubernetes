0336b6066e6b5b9c92d42e92cda2f1abcbfa5e44
Alan Christie <alan.christie@matildapeak.com>
2020-08-05 11:52:00 +0100

    - Initial file-set

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-cookiecutter-ansible-kubernetes
+# cookiecutter-ansible-kubernetes
+A [cookiecutter] template repository to create our style of AWX-compliant
+repositories - repositories that contain a playbook (and a role) to deploy a
+Kubernetes application.
+
+Use this cookiecutter to quickly template new Kubernetes application projects
+that you expect to deploy through AWX. The cookiecutter produces an Ansible
+playbook and role with some preset variables defined in `defaults` and `vars`
+with `tasks` to deploy the application and all its components from Ansible
+`templates`.
+        
+Answer the cookiecutter questions to create pre-formatted templates
+for the deployment of a container image application that can be a **CronJob**,
+**Deployment**, **Job** or **StatefulSet**. You can also decide whether you
+want a **PVC** (attached to the application) and an example **Secret** and
+**ConfigMap** to get you started.
+
+If it's a simple application it should be ready to add as a **Project** in AWX
+and run once you commit the new project to Git.
+
+Additionally, the generated project contains a `.travis.yml`
+to perform lint and ansible checking of the project.
+
+## Creating projects with the cookiecutter
+
+???
+
+---
+
+[cookiecutter]: https://cookiecutter.readthedocs.io
---
+++ hooks/post_gen_project.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import os
+import re
+from pathlib import Path
+
+# It's easier to let cookiecutter render the files
+# and then remove those that the user doesn't want.
+# Optionally including and excluding files
+# during the rendering process isn't
+# the way to do things.
+
+
+def delete_template_file(filename):
+    os.remove(os.path.join('roles',
+                           '{{cookiecutter.role_name|lower}}',
+                           'templates',
+                           filename + '.yaml.j2'))
+
+
+# Remove rendered files that the user doesn't want...
+
+if '{{cookiecutter.add_service|lower}}' in ['n']:
+    delete_template_file('service')
+
+if '{{cookiecutter.add_ingress|lower}}' in ['n']:
+    delete_template_file('ingress')
+    delete_template_file('rolebinding-default-sa')
+
+if '{{cookiecutter.add_pvc|lower}}' in ['n']:
+    delete_template_file('pvc')
+
+if '{{cookiecutter.add_configmap|lower}}' in ['n']:
+    delete_template_file('configmap')
+
+# Can only be one of 'deployment, job, statefulset.
+# Delete files that don't apply...
+
+if '{{cookiecutter.deployment}}' == 'cronjob':
+    for basename in ['deployment', 'job', 'statefulset']:
+        delete_template_file(basename)
+if '{{cookiecutter.deployment}}' == 'deployment':
+    for basename in ['cronjob', 'job', 'statefulset']:
+        delete_template_file(basename)
+elif '{{cookiecutter.deployment}}' == 'job':
+    for basename in ['cronjob', 'deployment', 'statefulset']:
+        delete_template_file(basename)
+elif '{{cookiecutter.deployment}}' == 'statefulset':
+    for basename in ['cronjob', 'deployment', 'job']:
+        delete_template_file(basename)
+
+# Now remove consecutive blank lines from all the files in 'roles'...
+#
+# A recurring problem with jinja2 and not being able to control it
+# with 'trim_blocks' until cookiecutter 2.x.
+# See https://github.com/cookiecutter/cookiecutter/issues/704
+#
+# For now this snippet replaces every file with one where multiple blank
+# lines are replaced by one...
+
+for path in Path('roles').glob('**/*'):
+    if os.path.isfile(path):
+        with open(path, 'rt') as source_file:
+            path_contents = source_file.read()
+        path_contents = re.sub('\n\n\n+', '\n\n', path_contents)
+        with open(path, 'wt') as source_file:
+            source_file.write(path_contents.strip() + '\n')
